f053c0439a12753a23578466dbf7d2f87f628955
Azzam <azzam49coder@gmail.com>
2021-01-23 10:38:01 +0800

    init

---
+++ core/blog/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+#foreign key has PROTECT that means when the category
+#gets deleted , its post won't get deleted
+class Post(models.Model):
+
+    # django uses managers to interface between model and
+    #the database, now instead of using only the default
+    # Post.objects. to return data we could use PostObjects
+    #which will return the specfic filters we applied to it,
+    #in our case is .filter(status='published')
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )9
+
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT,default=1
+    )
+    title = models.CharField(max_length=100)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    #unqiue_for_date='published' , this will tell django to now
+    #allow 2 records have the same slug value and same date of published field
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts'
+    )
+    status = models.CharField(
+        max_length=10, choices=options, default='published'
+    )
